5d80240c81e9ef5177bd475604efaa41285872a6
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-08-09 21:34:31 +0200

    Create signup view

---
+++ src/views/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+    <h3>Create a new account</h3>
+    <input v-model="email" type="text" placeholder="email" /><br/>
+    <input v-model="password" type="password" placeholder="password" /><br/>
+    <button @click="onSignUp">Sign up</button>
+    <p>Got an account? <router-link to="/sign-in">login here</router-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    onSignUp() {
+      this.$store.dispatch("signupUser", {
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
