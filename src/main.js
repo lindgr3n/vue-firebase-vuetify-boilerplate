9ebff00d748e986ad8e243798b49dc0ce7ecf90d
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-08-09 21:36:07 +0200

    Add auth check att startup

--- src/main.js
+++ src/main.js
@@ -2,11 +2,28 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase, { onAuthenticationChanged } from "./firebase";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  data: {
+    firebase
+  },
+  created() {
+    onAuthenticationChanged().then(user => {
+      if (user) {
+        console.log("Auth changed found user", user);
+
+        this.$store.dispatch("setUser", { user });
+        this.$router.push("/");
+      } else {
+        this.$router.push("/sign-in");
+      }
+      // this.$store.commit("setAppState", { type: "success" });
+    });
+  },
   render: h => h(App)
 }).$mount("#app");
