d7f2e7cecc901f4ce15e0a676a6789998b4812b0
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-09-04 21:19:56 +0200

    Refactor login

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,12 +1,49 @@
 <template>
-  <div class="login">
-    <h3>Sign in</h3>
-    <input v-model="email" type="text" placeholder="email" /><br/>
-    <input v-model="password" type="password" placeholder="password" /><br/>
-    <button @click="onLogin">Login</button>
-    <p v-if="error">{{error}}</p>
-    <p>You don't have an account? You can <router-link to="/sign-up">create one here</router-link></p>
-  </div>
+  <v-content>
+    <v-card class="elevation-12">
+      <v-toolbar dark color="primary">
+        <v-toolbar-title>Login form</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn fab small>Signup</v-btn>
+      </v-toolbar>
+      <v-container fluid fill-height>
+        <v-layout>
+          <v-flex>
+            <v-btn color="primary" round>
+              <v-icon>fab fa-twitter-square</v-icon>
+              <span>Twitter</span>
+            </v-btn>
+            <br />
+            <v-btn color="primary" round>
+              <v-icon>fab fa-facebook-square</v-icon>
+              <span>Facebook</span>
+            </v-btn>
+            <br />
+            <v-btn color="primary" round>
+              <v-icon>fab fa-github-square</v-icon>
+              <span>Github</span>
+            </v-btn>
+            <br />
+            <v-btn color="primary">
+              <v-icon>fab fa-google-plus-square</v-icon>
+              <span>Google</span>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+        <hr />
+        <v-layout>
+          <v-flex xs8>
+            <v-form>
+              <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
+              <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+            </v-form>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="onLogin">Login</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </v-content>
 </template>
 
 <script>
