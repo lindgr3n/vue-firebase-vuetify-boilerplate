7cbd60b416f1ac0b7b31441e7025c0d551a01e9e
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-09-06 21:01:08 +0200

    Better linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,16 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/prettier'
+    '@vue/prettier',
+    "plugin:vue/recommended"
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/name-property-casing": ["error", "kebab-case"],
+    "vue/require-default-prop": ["off"],
+    "curly": "error",
+    "no-console": "warn"
   },
   parserOptions: {
     parser: 'babel-eslint'
