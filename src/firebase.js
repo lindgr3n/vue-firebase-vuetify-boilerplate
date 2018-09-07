255bcb9fa00b8879eabce86964d8462a744190bb
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-09-07 21:31:57 +0200

    Just return error object

--- src/firebase.js
+++ src/firebase.js
@@ -197,15 +197,7 @@ export function signInWithGithub() {
           // ...
         })
         .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          console.error({ errorCode, errorMessage, email, credential });
-          reject({ errorCode, errorMessage, email, credential });
+          reject(error);
           // ...
         });
     }
