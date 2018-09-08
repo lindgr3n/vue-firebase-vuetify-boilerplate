1275c1f83a025830c4feba337ce7e9e68f7611c2
Johan Lindgren <johan.lindgren@c4contexture.com>
2018-09-08 20:42:30 +0200

    Added fetch method and update user info with info from db using fetch

--- src/store/model/user.js
+++ src/store/model/user.js
@@ -1,5 +1,11 @@
-import { signInUser, signupUser, signOutUser, addUser } from "@/firebase";
-import { signInWithGithub } from "../../firebase";
+import {
+  signInUser,
+  signupUser,
+  signOutUser,
+  addUser,
+  fetchUser,
+  signInWithGithub
+} from "@/firebase";
 
 const state = {
   user: null,
@@ -35,35 +41,41 @@ export const mutations = {
 };
 
 export const actions = {
-  USER_SIGNIN({ commit }, payload) {
+  USER_SIGNIN({ commit, dispatch }, payload) {
     commit("USER_REQUEST");
-    // TODO: Trigger application loading
     const loginPromise = signInUser(payload);
     loginPromise
       .then(user => {
-        commit("USER_UPDATE", { user });
-        commit("USER_SUCCESS");
+        dispatch("USER_FETCH", user);
       })
       .catch(error => {
         commit("USER_ERROR", { message: error.message });
       });
   },
 
-  USER_SIGNIN_GITHUB({ commit }) {
+  USER_SIGNIN_GITHUB({ commit, dispatch }) {
     commit("USER_REQUEST");
     const loginPromise = signInWithGithub();
     loginPromise
       .then(user => {
         console.log("Wohoo github", user);
-        commit("USER_UPDATE", { user });
-        commit("USER_SUCCESS");
+        const addUserPromise = addUser(user);
+        addUserPromise
+          .then(() => {
+            // TODO: Check if user exist before create?
+            // Could be nice to always add (merge) so we fetch user changes from social login
+            dispatch("USER_FETCH", user);
+          })
+          .catch(error => {
+            commit("USER_ERROR", { message: error.message });
+          });
       })
       .catch(error => {
         commit("USER_ERROR", { message: error.message });
       });
   },
 
-  USER_SIGNUP({ commit }, { email, password }) {
+  USER_SIGNUP({ commit, dispatch }, { email, password }) {
     commit("USER_REQUEST");
     const createdUserPromise = signupUser({ email, password });
     createdUserPromise
@@ -72,8 +84,7 @@ export const actions = {
         const addUserPromise = addUser(user);
         addUserPromise
           .then(() => {
-            commit("USER_UPDATE", { user });
-            commit("USER_SUCCESS");
+            dispatch("USER_FETCH", user);
           })
           .catch(error => {
             commit("USER_ERROR", { message: error.message });
@@ -81,6 +92,19 @@ export const actions = {
       })
       .catch(error => {
         commit("USER_ERROR", { message: error.message });
+      });
+  },
+
+  USER_FETCH({ commit }, { user }) {
+    commit("USER_REQUEST");
+    const fetchedUserPromise = fetchUser(user);
+    fetchedUserPromise
+      .then(user => {
+        commit("USER_UPDATE", { user });
+        commit("USER_SUCCESS");
+      })
+      .catch(error => {
+        commit("USER_ERROR", error.message);
       });
   },
 
